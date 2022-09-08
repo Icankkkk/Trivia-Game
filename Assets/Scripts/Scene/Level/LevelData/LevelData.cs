603306d70387d8703a15e5ffac2653f368dd1134
winnerinsanjaya <61342260+Icankkkk@users.noreply.github.com>
2022-09-08 16:35:36 +0800

    Update Level Module
    - Create ButtonLevel prefab
    - Create LevelScene.cs
    - Create LevelData.cs

---
+++ Assets/Scripts/Scene/Level/LevelData/LevelData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Trivia.Scene.Level.LevelData
+{
+    public class LevelData : MonoBehaviour
+    {
+
+    }
+}
+
+
---
+++ Assets/Scripts/Scene/Level/LevelScene/LevelScene.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+using Trivia.Global.Database;
+
+namespace Trivia.Scene.Level.LevelScene
+{
+    public class LevelScene : MonoBehaviour
+    {
+        [SerializeField] private Button _backButton;
+        [SerializeField] private TextMeshProUGUI _labelText;
+
+        private void Awake()
+        {
+            _backButton.onClick.RemoveAllListeners();
+            _backButton.onClick.AddListener(GoSelectPack);
+        }
+
+        private void Start()
+        {
+            SetLabelText();
+        }
+
+        private void GoSelectPack()
+        {
+            Debug.Log("Load to Pack Scene!");
+            SceneManager.LoadScene("Pack");
+        }
+
+        private void SetLabelText()
+        {
+            Database database = FindObjectOfType<Database>();
+            string levelName = database._currentPack;
+            _labelText.text = "Level " + levelName;
+        }
+    }
+
+}
